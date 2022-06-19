bd89413afa85dad39a5c73b5091a7456d4f465b0
Joseph <josephputz89@gmail.com>
2022-06-19 10:05:01 -0500

    commit tree c5f714828405c23250fcc10dbafdeb18bc91eef9

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,25 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Movies</h1>
+      {movies.map((movie) => (
+        <div key={movie.title}>
+          <h2>{movie.title}</h2>
+          <h2>{movie.time}</h2>
+          <ul>
+          {movie.genres.map((genre) => <li key={genre}>{genre}</li>)}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Movies;
+
+// title: "Jack Reacher: Never Go Back",
+// time: 118,
+// genres: ["Action", "Adventure", "Crime", "Mystery", "Thriller"],
+// },
